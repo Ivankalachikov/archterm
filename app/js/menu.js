aeaae1c142eb8e98fc08f289c86191a90bb80abf
Ivan <ivan.kalachikov@gmail.com>
2016-06-18 13:50:54 +0700

    fix any bug

--- app/js/menu.js
+++ app/js/menu.js
@@ -1,34 +1,3 @@
-// document.addEventListener('DOMContentLoaded', function () {
-
-// 	var icon = document.querySelector('.page-nav__icon');
-// 	var menu = document.querySelector('.page-nav__list');
-// 	resize();
-
-// 	icon.addEventListener('click', function () {
-// 		menu.classList.toggle('hidden');
-// 	});
-
-// 	// document.addEventListener('click', function (event){
-// 	// 	if (event.target.closest('.page-nav__icon') == null){
-// 	// 		menu.classList.add('hidden');
-// 	// 	}
-// 	// });
-
-// 	function resize() { 
-//     var ww = window.innerWidth;
-//     if (ww>991){
-//     	menu.classList.remove('hidden');
-//     	icon.classList.add('hidden');
-//     }
-//     else{
-//     	menu.classList.add('hidden');
-//     	icon.classList.remove('hidden');
-//     }
-//   }
-
-//   window.onresize = resize;
-
-// });
 
 $(document).ready(function(){
 
@@ -44,7 +13,7 @@ $(document).ready(function(){
 		if ($(window).width()>991){
 			$('.page-nav__list').show();
 		} else {
-			$('.page-nav__list').hide();
+			//$('.page-nav__list').hide();
 		}
 	})
 })
